b73775669e21b2c02906392a9a23815d64cc3d44
Jactics <jactiction@gmail.com>
2019-01-06 20:40:23 +0800

    comment1

--- Pythontest1/Pythontest1.py
+++ Pythontest1/Pythontest1.py
@@ -1,2 +1,61 @@
+import pymssql
+import mpl_finance
 
-print("This is the test12")
+class Candle1():
+    def __init__(self, open1, high1, low1, close1):
+        self.open = open1
+        self.high = high1
+        self.low = low1
+        self.close = close1
+
+    def printopen(self):
+        return self.open
+
+
+my_candle1 = Candle1(10,20,30,40)
+my_candle2 = Candle1(100,200,300,400)
+candlelist=[]
+candlelist.append(my_candle1)
+candlelist.append(my_candle2)
+candlelist[1].printopen()
+
+conn = pymssql.connect(host=r'DESKTOP-8OMB7OV\SQLEXPRESS',user = 'sa', password ='qshmxh',database ='dbcontact',charset='utf8')
+print(conn)
+cursor = conn.cursor()
+#sql = 'select * from Contacts'
+sql = "insert into Contacts () values()"
+cursor.execute(sql)
+#rs = cursor.fetchall()
+rs = cursor.fetchone()
+cursor.close()
+conn.close()
+print(rs)
+
+with pymssql.connect(host=r'DESKTOP-8OMB7OV\SQLEXPRESS',user = 'sa', password ='qshmxh',database ='dbcontact',charset='utf8') as conn:
+    with conn.cursor() as cursor:
+        sql = 'select * from Contacts'
+        cursor.execute(sql)
+        rs = cursor.fetchall()
+        for row in rs:
+            print(row)
+        pass
+    pass
+
+
+
+
+with pymssql.connect(host=r'DESKTOP-8OMB7OV\SQLEXPRESS',user = 'sa', password ='qshmxh',database ='dbcontact',charset='utf8') as conn:
+    with conn.cursor() as cursor:
+        test1 = '5266252'
+        sql = "insert into Contacts (ContactName,Email,PhoneNumber,Address) values(%s,%s,%s,%s)"
+        sqllist = ('Johnson','johnson@gmail.com',test1,'dac 14')
+        cursor.execute(sql,sqllist)
+
+        #sql = "insert into Contacts(ContactName,Email,PhoneNumber,Address) values(%d,%s,%s,%s)"
+        #sqllist =[('Johnson','johnson@gmail.com','5266252','dac14'),('kitty','kitty@gmail.com','5152','ddw14')]
+        #cursor.executemany(sql,sqllist)
+
+        #sql = "Update Contacts set PhoneNumber = %s where Id = %d"
+        #cursor.execute(sql,('256125',7))
+        conn.commit()
+
--- Pythontest1/module1.py
+++ Pythontest1/module1.py
@@ -1,3 +1,172 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+import mpl_finance as mpf
+import matplotlib.ticker as ticker
+import matplotlib.dates as mdates
+import pymssql
+DateTime = []
+Open = []
+Close = []
+High = []
+Low = []
+#Time = []
+CandleVolume =[]
+CandleOI = []
+Timestr = []
 
-print("test2")
-print("Hi")
+
+tradedatetime = []
+tradeprice = []
+tradeposition = []
+Buydatetime = []
+Selldatetime = []
+Buyposition = []
+Sellposition =[]
+Buyprice = []
+Sellprice = []
+
+#TimeCandlelist = []
+
+
+def loadTimetext(textpath):
+    Open.clear()
+    DateTime.clear()
+    Close.clear()
+    High.clear()
+    Low.clear()
+    CandleVolume.clear()
+    #CandleOI.clear()
+    #pullResult = open(textpath,'r').read()
+    pullcsv = open(textpath,'r')
+    eline = pullcsv.readline()
+    #dataArray = pullResult.split('\n')
+    while(eline):
+        eline = pullcsv.readline()
+        if len(eline)>5:
+            line  = eline.split(',')
+            t1 = dt.datetime.strptime(line[0]+' '+line[1], '%m/%d/%Y %H:%M:%S.%f')
+            DateTime.append(t1)
+            Open.append(float(line[2]))
+            High.append(float(line[3]))
+            Low.append(float(line[4]))
+            Close.append(float(line[5]))
+            CandleVolume.append(int(line[6]))
+            #CandleOI.append(int(line[6]))
+    pass
+
+def loadtradetext(textpath):
+    tradedatetime.clear()
+    tradeprice.clear()
+    tradeposition.clear()
+    pullcsv = open(textpath,'r')
+    eline = pullcsv.readline()
+    while(eline):
+        eline = pullcsv.readline()
+        if len(eline)>5:
+            line  = eline.split('\t')
+            t1 = dt.datetime.strptime(line[13]+' '+line[14][:-3],'%Y%m%d %H:%M')
+            #tradedatetime.append(t1)
+            if(line[7]=='买'):
+                #tradeposition.append(int(line[11]))
+                Buydatetime.append(t1)
+                Buyposition.append(int(line[11]))
+                Buyprice.append(float(line[10]))
+            elif(line[7]=='卖'):
+                #tradeposition.append(-int(line[11]))
+                Selldatetime.append(t1)
+                Sellposition.append(int(line[11]))
+                Sellprice.append(float(line[10]))
+            else:
+                #tradeposition.append(0)
+                pass
+            #tradeprice.append(float(line[10]))
+    pass
+
+def text2db(textpath):
+    pullcsv = open(textpath,'r')
+    eline = pullcsv.readline()
+    with pymssql.connect(host=r'127.0.0.1',user = 'sa', password ='qshmxh',database ='dbcldata',charset='utf8') as conn:
+        with conn.cursor() as cursor:
+            while(eline):
+                eline = pullcsv.readline()
+                if len(eline)>5:
+                    line  = eline.split(',')
+                    t1 = dt.datetime.strptime(line[0]+' '+line[1], '%m/%d/%Y %H:%M:%S.%f')
+                    strt1 = t1.strftime('%Y-%m-%d %H:%M:%S')
+                    dataopen = float(line[2])
+                    datahigh = float(line[3])
+                    datalow  = float(line[4])
+                    dataclose = float(line[5])
+                    datavolume = int(line[6])
+                    sql = "insert into CL18021811 (_datetime,_open,_high,_low,_close,_volume) values (%s,%d,%d,%d,%d,%d)"
+                    sqltuple = (strt1,dataopen,datahigh,datalow,dataclose,datavolume)
+                    cursor.execute(sql,sqltuple)
+                    conn.commit()
+    pass
+
+def plotTimeCandle():
+    #fig = plt.figure(figsize =(8,6))
+    plt.clf
+    fig=plt.figure(figsize=(8,6))
+    ax = fig.add_axes([0.1,0.35,0.8,0.5])
+    #ax2 = fig.add_axes([0.1,0.15,0.8,0.2])
+    ax.yaxis.tick_right()
+    #ax2.yaxis.tick_right()
+
+    quotes = mdates.date2num(DateTime), Open, High, Low, Close
+    quotes = list(map(list,zip(*quotes)))
+    mpf.candlestick_ohlc(ax,quotes,width =0.0001,colorup='green',colordown='red',alpha=1)
+
+    #mpf.candlestick2_ochl(ax,Open,Close,High,Low,width =0.5,colorup='green',colordown='red',alpha=1)
+    #ax.set_xticklabels(DateTime,rotation =90)
+    #ax.set_xticks(range(0,len(DateTime),1))
+    #mpf.volume_overlay(ax2, Open, Close, CandleVolume, colorup='green', colordown='red', width=0.5, alpha=0.8)
+    #ax2.set_xticklabels(DateTime,rotation ="vertical")
+    #ax2.set_xticks(range(0,len(DateTime),1))
+    #ax.scatter(tradedatetime,tradeprice,s=20,c=tradeposition,marker='o')
+    ax.scatter(Buydatetime,Buyprice,s=20*Buyposition,c='blue',marker='o')
+    ax.scatter(Selldatetime,Sellprice,s=20*Sellposition,c='red',marker='o')
+    locator = mdates.MinuteLocator(byminute = [0,30])
+    locator.MAXTICKS =40000
+    ax.xaxis.set_major_locator(locator)
+    majorFmt = mdates.DateFormatter('%m-%d %H:%M:%S')  
+    ax.xaxis.set_major_formatter(majorFmt)
+
+    #locator = ticker.MultipleLocator(6)
+    #locator.MAXTICKS = 140000
+    #ax.xaxis.set_major_locator(locator)
+    #ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+    #ax.set_xlim([DateTime[0],DateTime[-1]])
+    xlabels = ax.get_xticklabels()
+    #plt.subplots_adjust(hspace=0,bottom =0.2)
+    plt.setp(xlabels, rotation = 60)
+    #ax.scatter(tradedatetime,tradeprice,s=20,c=tradeposition,marker='o')
+    #ax.legend(loc='upper left')
+    #plt.grid()
+    
+    plt.show()
+    
+    pass
+
+#def format_date(x,pos=None):
+#    if x<0 or x>len(DateTime)-1:
+#        return ''
+#    return DateTime[int(x)]
+#pass
+
+def plotTradeRecord():
+    plt.clf
+    fig=plt.figure(figsize=(8,6))
+    ax = fig.add_axes([0.1,0.35,0.8,0.5])
+    ax.scatter(tradedatetime,tradeprice,s=20,c=tradeposition,marker='o')
+    ax.set_xlim([tradedatetime[0],tradedatetime[-1]])
+    plt.show()
+    pass
+
+
+text2db(r'D:\Research\CME.CL HOT.csv')
+#loadTimetext(r'D:\Research\test1.txt')
+#loadtradetext(r'D:\Research\tradingtest1.txt')
+#loadTimetext(r'D:\Research\CME.CL HOT.csv')
+#plotTimeCandle()
+#plotTradeRecord()
